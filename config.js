e58cb10a997b47abc82560b48ddb30f4f1ed54d4
Ralph Holzmann <ralphholzmann@gmail.com>
2012-05-14 08:57:37 -0500

    Adding config file for steal amd tests.

---
+++ config.js
@@ -0,0 +1,20 @@
+/* 
+Note: When running the tests for the lsjs loader make sure localStorage has been
+cleared for the first run to ensure the latest version of the tests are executed.
+Either clear the browser cookies or programatically run localStorage.clear();
+*/
+var config = config,
+	go = require,
+	implemented = {
+/** /
+		basic: true,
+		anon: true,
+		funcString: true,
+		namedWrapped: true,
+		require: true,
+/**/
+		plugins: true
+/** /
+		//pluginDynamic: true
+		/**/
+	};
